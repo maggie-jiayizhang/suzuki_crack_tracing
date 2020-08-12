7722ebf13189a7f97d1241ec0f1f825c3807cfd7
Jiayi Zhang <jiayizha@andrew.cmu.edu>
2020-08-11 23:01:03 -0400

    Edited README file, updated suzuki.m

--- README.md
+++ README.md
@@ -1,1 +1,41 @@
 # suzuki_crack_tracing
+
+This is a crack tracing project (primarily for images in [Suzuki et al. 2020] (https://doi.org/10.1038/s42003-020-0860-1), see images in inputs/) by Maggie Zhang, MGRGX20.
+
+**LANGUAGE: MATLAB**
+
+*Key words*: Dynamic programming (DP), Suzuki et al. 2020, geoscience
+
+## Usage
+For an input fluorescent microscopy image of stained microbes (pre-cropped), this pipeline first turns the image to black&white. Further denoising is optional at this point. Then the pipeline either proceed with/out skeletonizing the image. *A line marking the edge of the crack (OR where most microbial signal density) is present* will be generated as one of the end results of the pipeline. The pipeline also calculates *%signal:non-signal* on the line as well as *ratio of area intersecting the line/total signal area*.
+
+For high level explanation of how the algorithm works, go to this [google doc].
+(https://docs.google.com/document/d/19VloN7BrtaRdAaxrYXAAJ4M6lErzpptEheu-elLCJMM/edit?usp=sharing)
+
+## Directory Description
+
+- **suzuki_crack_tracing**
+    - **inputs**
+        - a.png: cropped img a from original.png
+        - a_edited.png: manually edited a.png to hide noise signal at the bottom
+          (Note: this step is not necessary)
+    - *suzuki.m*: the whole pipeline
+    - *score_path.m*: defines score_path()
+    - *generate_path.m*: defines generate_path()
+    - *get_area.m*: defines get_area(), helper function
+    - *find_intersect.m*: defines find_intersect()
+    - *remove_by_area.m*: defines remove_by_area, optional denoise func
+    - **outputs**: preview of outputs in the pipeline + manually traced image
+    - original.png: original img from Suzuki et al. 2020
+
+## Notes
+1. You can run suzuki.m in MATLAB with
+    $ suzuki
+
+2. You will need to replace the image path on line 4 of *suzuki.m* to process the image you want.
+
+3. 4 figures will be opened at the end to show the outputs at different stages in the pipeline. Toggle the code blocks for visualization off if any figure is not needed.
+
+4. Currently the pipeline is set to run on both the skeletonized and the b&w (non-skeletonized) image. You can comment out/toggle the code block (in Method1 or Method2) to run only the portion you want.
+
+5. Calculation are printed to MATLAB window automatically.
--- suzuki.m
+++ suzuki.m
@@ -1,10 +1,10 @@
 % Main pipeline
 
 % read image
-a = imread("a_edited.png");
+a = imread('inputs/a_edited.png');
 %turn original image to black & white (bw)
 t = graythresh(a);
-a_bw = im2bw(a, t); 
+a_bw = im2bw(a, t);
 %get the size of the original image
 [height, width] = size(a_bw);
 % uncomment to show the product of this stage.
@@ -12,21 +12,21 @@ a_bw = im2bw(a, t);
 
 
 %%%%%%%%%%%%%%%%%%%%%%%%% Method 1 %%%%%%%%%%%%%%%%%%%%%%%%%
-% first trace the signals (blobs); "skeletonize" the edge; generate path
+% first trace the signals (blobs); 'skeletonize' the edge; generate path
 
 % use Sobel's kernel to find the vertical edges in the image
 % (we don't really care about the horizontal edges)
-sx = fspecial("sobel");
+sx = fspecial('sobel');
 sy = sx';
 % set sx to zeros matrix -> ignore horizontal edges
 sx = zeros(3, 3);
-gx = imfilter(a_bw, sx, "replicate");
-gy = imfilter(a_bw, sy, "replicate");
+gx = imfilter(a_bw, sx, 'replicate');
+gy = imfilter(a_bw, sy, 'replicate');
 grad = sqrt(gx.*gx + gy.*gy); % grad = gradient
 % uncomment to show the product of this stage.
 % imshow(grad);
 
-% "skeletonize" the traced image
+% 'skeletonize' the traced image
 grad_skel = bwmorph(grad, 'skel', Inf);
 
 % % further denoise (optional)
@@ -44,8 +44,9 @@ P_skel = mat2gray(score_skel);
 
 % trace back with the scoring matrix to generate the best path.
 [path_skel, count_skel] = generate_path(grad_skel, score_skel, sr, 1, 0.2);
-total = get_area(path_skel, 1, 1);
+[r_skel, c_skel] = find(path_skel==1);
 
+total = get_area(path_skel, 1, 1);
 percent1 = count_skel/total
 %%%%%%%%%%%%%%%%%%%%%%%%% Method 1 ends %%%%%%%%%%%%%%%%%%%%%%%%%
 
@@ -59,8 +60,9 @@ P_bw = mat2gray(score_bw);
 
 % trace back with the scoring matrix to generate the best path.
 [path_bw, count_bw] = generate_path(a_bw, score_bw, sr, 1, 0.2);
-total = get_area(path_bw, 1, 1);
+[r_bw, c_bw] = find(path_bw==1);
 
+total = get_area(path_bw, 1, 1);
 percent2 = count_bw/total
 %%%%%%%%%%%%%%%%%%%%%%%%% Method 2 ends %%%%%%%%%%%%%%%%%%%%%%%%%
 
@@ -71,33 +73,33 @@ percent2 = count_bw/total
 % FIGURE 1 shows original images.
 figure;
 subplot(1,2,1);imshow(a);
-title("Original image (bottom covered) from Suzuki 2020: a");
+title('Original image (bottom covered) from Suzuki 2020: a');
 subplot(1,2,2);imshow(a_bw);
-title("Black & white ver of the original image: a\_bw");
+title('Black & white ver of the original image: a\_bw');
 
 % FIGURE 2 shows the results of edge tracing + cleaning.
 figure;
 subplot(1,2,1);imshow(grad);
-title("Annotations of vertical edges in a\_bw: grad");
+title('Annotations of vertical edges in a\_bw: grad');
 subplot(1,2,2);imshow(grad_skel);
-title("Denoised/skeletonized version of grad: grad\_skel");
+title('Denoised/skeletonized version of grad: grad\_skel');
 
 % FIGURE 3 shows the visualization of scoring matrices.
 figure;
-title("Visualization of scores. Lighter pixel = higher score");
+title('Visualization of scores. Lighter pixel = higher score');
 subplot(1,2,1);imshow(P_skel);
-title("Visualization on grad\_skel (method 1)");
+title('Visualization on grad\_skel (method 1)');
 subplot(1,2,2);imshow(P_bw);
-title("Visualization on a\_bw (method 2)");
+title('Visualization on a\_bw (method 2)');
 
 % FIGURE 4 shows the final paths plotted on a_bw.
 figure;
 subplot(1,2,1);imshow(a_bw);
-title(["Crack tracing on processed image, %signal=", percent1]);
+title(['Crack tracing on processed image, %signal=', percent1]);
 hold on;
 plot(c_skel, r_skel, 'b.', 'MarkerSize', 0.1);
 subplot(1,2,2);imshow(a_bw);
-title(["Crack tracing on original black & white image, %signal=", percent2]);
+title(['Crack tracing on original black & white image, %signal=', percent2]);
 hold on;
 plot(c_bw, r_bw, 'r.', 'MarkerSize', 0.1);
 impixelinfo;
@@ -105,7 +107,7 @@ impixelinfo;
 
 %%%%%%%%%%%%%%%%%%%%%%%%% Find Intersection %%%%%%%%%%%%%%%%%%%%%%%%%
 % find connected components, aka. signal areas on the image.
-% (note: the next line is also in the "further denoise" block above)
+% (note: the next line is also in the 'further denoise' block above)
 [L, n] = bwlabel(a_bw, 8);
 
 % find area intersecting the line (path = path_skel)
